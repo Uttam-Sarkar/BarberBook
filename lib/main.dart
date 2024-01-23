1c872cfc5fee38911f8f0c1b09215491eaa0c9e8
Uttam-Sarkar <s2010876149ac.ru.bd>
2024-01-23 15:16:11 +0600

    roleWise  log in work perfectly

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
-import 'dart:js';
-
+//import 'dart:js';
 import 'package:barberbook/firebase_options.dart';
 import 'package:barberbook/serviceProviderScreen.dart';
 import 'package:barberbook/userScreen.dart';
@@ -8,13 +7,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'login.dart';
-Future<void> main() async{
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -28,25 +29,30 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: SplashPage(),
+      home:const SplashPage(),
     );
   }
 }
+
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
   State<SplashPage> createState() => SplashPageState();
 }
+
 //_SplashPageStateState eta private.. '_' na thakle static
 class SplashPageState extends State<SplashPage> {
-
   static const String KEYLOGIN = "login";
   static const String ROLE = "role";
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    // Timer(Duration(seconds: 5), () {
+    //   Navigator.pushReplacement(context,
+    //   MaterialPageRoute(builder: (context) => MyLogin()));
+    // });
     whereToGo();
   }
 
@@ -54,7 +60,8 @@ class SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-
+        color: Colors.blue,
+        child:Icon(Icons.account_circle, color: Colors.white,),
       ),
     );
   }
@@ -65,26 +72,34 @@ class SplashPageState extends State<SplashPage> {
     var isLoggedIn = sharePref.getBool(KEYLOGIN);
     var role = sharePref.getString(ROLE);
 
-    Timer(Duration(seconds: 2), (){
-
-      if( isLoggedIn != null && role !=null){
-        if(isLoggedIn){
-          //role base log in
-          if(role == "ServiceProvider"){
-            Navigator.pushReplacement(context as BuildContext,
-                MaterialPageRoute(builder: (context) => ServiceProviderScreen(),));
+    Timer(Duration(seconds: 2),() {
+        if (isLoggedIn != null && role != null ) {
+          if (isLoggedIn) {
+            //role base log in
+            if (role == "ServiceProvider") {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(
+                    builder: (context) => ServiceProviderScreen(),
+                  ));
+            } else {
+              Navigator.pushReplacement( context,
+                  MaterialPageRoute(
+                    builder: (context) => UserScreen(),
+                  ));
+            }
+          } else {
+            Navigator.pushReplacement( context,
+                MaterialPageRoute(
+                  builder: (context) => MyLogin(),
+                ));
           }
-          else{
-            Navigator.pushReplacement(context as BuildContext,
-                MaterialPageRoute(builder: (context) => UserScreen(),));
-          }
-
         } else{
-          Navigator.pushReplacement(context as BuildContext,
-              MaterialPageRoute(builder: (context) => MyLogin(),));
+          Navigator.pushReplacement( context,
+              MaterialPageRoute(
+                builder: (context) => MyLogin(),
+              ));
         }
-      }
-
-    },);
+      },
+    );
   }
 }
